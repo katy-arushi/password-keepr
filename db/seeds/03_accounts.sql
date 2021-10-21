b5ac57bc07dd021b498a668ac0b47f860a2b319c
Arushi Katyal <katy.arushi@gmail.com>
2021-10-21 01:47:30 -0400

    add accounts seeds

---
+++ db/seeds/03_accounts.sql
@@ -0,0 +1,6 @@
+INSERT INTO accounts (org_id, category_id, website_name, website_url, login, password)
+VALUES(1, 1, 'Facebook', 'www.facebook.com', 'mimi.chan@email.com', 'facebookpassword1'),
+(1, 2, 'LinkedIn', 'www.linkedin.com', 'sebatian.mcdonald@email.com', 'password123'),
+(2, 3, 'Netflix', 'www.netflix.com', 'lady.gaga@email.com', 'mypassabc'),
+(3, 4, 'MyYoga', 'www.yoga.com', 'alice.smith@email.com', 'yogagirl123'),
+(2, 2, 'BambooHR', 'www.hr.com', 'clover.grey@email.com', 'worktime789');

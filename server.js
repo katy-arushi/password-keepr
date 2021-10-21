17848a3e828667b5a7ae136f38f2ace693640a6a
OliviaWong-dev <olivia.wy.wong@gmail.com>
2021-10-21 22:29:51 +0000

    create login page

--- server.js
+++ server.js
@@ -49,6 +49,10 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.get("/index", (req, res) => {
   res.render("index");
 });
 

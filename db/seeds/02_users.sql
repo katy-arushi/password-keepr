29a33ba6964c97f351a06db1beca05baf6f626f2
Arushi Katyal <katy.arushi@gmail.com>
2021-10-21 01:39:48 -0400

    fix formatting error

--- db/seeds/02_users.sql
+++ db/seeds/02_users.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (org_id, first_name, last_name, password, email)
 VALUES(1, 'Mimi', 'Chan', 'password1', 'mimi.chan@email.com'),
 (1, 'Sebatian', 'McDonald', 'password2', 'sebatian.mcdonald@email.com'),
-(2, 'Lady', 'Gaga', 'password3', 'lady.gaga@email.com');
-(3, 'Alice', 'Smith', 'password4', 'alice.smith@email.com');
+(2, 'Lady', 'Gaga', 'password3', 'lady.gaga@email.com'),
+(3, 'Alice', 'Smith', 'password4', 'alice.smith@email.com'),
 (2, 'Clover', 'Grey', 'password5', 'clover.grey@email.com');

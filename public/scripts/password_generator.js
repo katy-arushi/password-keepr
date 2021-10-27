693ef2ea8922391bd2d0fb2610cd96c7003c1a62
OliviaWong-dev <olivia.wy.wong@gmail.com>
2021-10-27 16:39:26 +0000

    implement generate password logic

--- public/scripts/password_generator.js
+++ public/scripts/password_generator.js
@@ -1,23 +1,55 @@
 $(document).ready(function () {
-  function generateLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-  }
-  function generateUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-  }
-  function generateNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-  }
-  function generateSymbol() {
-    return String.fromCharCode(Math.floor(Math.random() * 14) + 33);
-  }
+  const charCodeRange = function (startRange, endRange) {
+    let range = [];
+    for (let i = startRange; i < endRange; i++) {
+      range.push(i);
+    }
+    return range;
+  };
+
+  let lowerCase = charCodeRange(97, 122);
+  let upperCase = charCodeRange(65, 90);
+  let numberCase = charCodeRange(48, 57);
+  let symbolCase = charCodeRange(33, 47);
+
+  // function generateSymbol() {
+  //   return String.fromCharCode(Math.floor(Math.random() * 14) + 33);
+  // }
 
   $("#password_gen").submit(function (event) {
     event.preventDefault();
-    console.log($(this).find("#passwordLength").val());
-    console.log($(this).find("#upperCaseBox").is(":checked"));
-    console.log($(this).find("#lowerCaseBox").is(":checked"));
-    console.log($(this).find("#numbersBox").is(":checked"));
-    console.log($(this).find("#symbolsBox").is(":checked"));
+    let passwordLength = $(this).find("#passwordLength").val();
+    let upper = $(this).find("#upperCaseBox").is(":checked");
+    let lower = $(this).find("#lowerCaseBox").is(":checked");
+    let number = $(this).find("#numbersBox").is(":checked");
+    let symbol = $(this).find("#symbolsBox").is(":checked");
+
+    $('input[name="generated_password"').empty;
+    let charCodeArr = [];
+    if (upper) {
+      charCodeArr.push(upperCase);
+    }
+    if (lower) {
+      charCodeArr.push(lowerCase);
+    }
+    if (number) {
+      charCodeArr.push(numberCase);
+    }
+    if (symbol) {
+      charCodeArr.push(symbolCase);
+    }
+    console.log(charCodeArr.flat());
+    const flatArray = charCodeArr.flat();
+    let password = [];
+    for (let i = 0; i < passwordLength; i++) {
+      password.push(
+        String.fromCharCode(
+          flatArray[Math.floor(Math.random() * flatArray.length)]
+        )
+      );
+    }
+    $('input[name="generated_password"').val(
+      `Your new password is: ${password.join("")}`
+    );
   });
 });

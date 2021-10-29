114c6c80d8bb515e0f2b34d734ad7e5c7f6b4649
OliviaWong-dev <olivia.wy.wong@gmail.com>
2021-10-29 21:17:35 +0000

    fix edit and clipboard

--- routes/accounts.js
+++ routes/accounts.js
@@ -9,109 +9,108 @@ module.exports = (db) => {
   // -------------------------------------- GET ROUTE HANDLERS -------------------------------------- //
 
   // GET accounts
-  router.get("/accounts", (req, res) => {  // renders HEADER partial
+  router.get("/accounts", (req, res) => {
+    // renders HEADER partial
     const userId = req.session.userId; // userID from cookies
 
     const templateVars = {};
 
-    db.query( // Query for displaying user's name in header
+    db.query(
+      // Query for displaying user's name in header
       `SELECT users.first_name AS name FROM users WHERE users.id = $1`,
       [userId]
-      )
-      .then((data) => {
-        const userName = data.rows;
-        templateVars.userName = userName; // add to template vars
+    ).then((data) => {
+      const userName = data.rows;
+      templateVars.userName = userName; // add to template vars
 
-        db.query(
-          `SELECT accounts.*, categories.website_category AS category FROM accounts
+      db.query(
+        `SELECT accounts.*, categories.website_category AS category FROM accounts
         JOIN categories ON categories.id = accounts.category_id
         JOIN organizations ON organizations.id = accounts.org_id
         JOIN users ON users.org_id = organizations.id
         WHERE users.id = $1`,
-          [userId]
-        )
-          .then((data) => {
-            const accounts = data.rows;
-            templateVars.accounts = accounts; // add to template vars
-            console.log(templateVars);
-            res.render("accounts", templateVars);
-          })
-          .catch((err) => {
-            res.status(500).json({ error: err.message });
-          });
-      })
-
+        [userId]
+      )
+        .then((data) => {
+          const accounts = data.rows;
+          templateVars.accounts = accounts; // add to template vars
+          console.log(templateVars);
+          res.render("accounts", templateVars);
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.message });
+        });
+    });
   });
 
-
   // GET new account
-  router.get("/accounts/new_account", (req, res) => {  // renders HEADER partial
+  router.get("/accounts/new_account", (req, res) => {
+    // renders HEADER partial
     const userId = req.session.userId; // userID from cookies
     const templateVars = {};
 
-    db.query( // Query for displaying user's name in header
+    db.query(
+      // Query for displaying user's name in header
       `SELECT users.first_name AS name FROM users WHERE users.id = $1`,
       [userId]
-      )
-      .then((data) => {
-        const userName = data.rows;
-        templateVars.userName = userName; // add to template vars
+    ).then((data) => {
+      const userName = data.rows;
+      templateVars.userName = userName; // add to template vars
 
-        db.query(`SELECT * FROM categories`)
-          .then((data) => {
-            const categories = data.rows;
-            templateVars.categories = categories; // add to template vars
-            res.render("new_account", templateVars);
-          })
-          .catch((err) => {
-            res.status(500).json({ error: err.message });
-          });
-      })
+      db.query(`SELECT * FROM categories`)
+        .then((data) => {
+          const categories = data.rows;
+          templateVars.categories = categories; // add to template vars
+          res.render("new_account", templateVars);
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.message });
+        });
+    });
   });
-
 
   // GET generate password
-  router.get("/accounts/generate_password", (req, res) => {  // renders HEADER partial
+  router.get("/accounts/generate_password", (req, res) => {
+    // renders HEADER partial
     const userId = req.session.userId;
-    db.query( // Query for displaying user's name in header
+    db.query(
+      // Query for displaying user's name in header
       `SELECT users.first_name AS name FROM users WHERE users.id = $1`,
       [userId]
-      )
-      .then((data) => {
-        const userName = data.rows;
-        const templateVars = { userName }; // make template vars obj
-        console.log(templateVars);
-        res.render("generate_password", templateVars);
-      })
+    ).then((data) => {
+      const userName = data.rows;
+      const templateVars = { userName }; // make template vars obj
+      console.log(templateVars);
+      res.render("generate_password", templateVars);
+    });
   });
 
-
   // GET edit_password
-  router.get("/accounts/:accountId", (req, res) => { // renders HEADER partial
+  router.get("/accounts/:accountId", (req, res) => {
+    // renders HEADER partial
     const accountId = req.params.accountId; // get accountID from the URL
     const userId = req.session.userId; // userID from cookies
-    const templateVars = {id: userId};
+    const templateVars = { id: accountId };
 
-    db.query( // Query for displaying user's name in header
+    db.query(
+      // Query for displaying user's name in header
       `SELECT users.first_name AS name FROM users WHERE users.id = $1`,
       [userId]
-      )
-      .then((data) => {
-        const userName = data.rows;
-        templateVars.userName = userName; // add to template vars
+    ).then((data) => {
+      const userName = data.rows;
+      templateVars.userName = userName; // add to template vars
 
-        db.query(`SELECT * FROM accounts WHERE id = $1`, [accountId])
-          .then((data) => {
-            const editedPassword = data.rows[0];
-            templateVars.editedPassword = editedPassword; // add to template vars
-            console.log(templateVars);
-            res.render("edit_password", templateVars);
-          })
-          .catch((err) => {
-            res.status(500).json({ error: err.message });
-          });
-      })
-
+      db.query(`SELECT * FROM accounts WHERE id = $1`, [accountId])
+        .then((data) => {
+          const editedPassword = data.rows[0];
+          templateVars.editedPassword = editedPassword; // add to template vars
+          console.log(templateVars);
+          res.render("edit_password", templateVars);
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.message });
+        });
+    });
   });
 
   // ------------------------------------ POST ROUTE HANDLERS --------------------------------------- //
@@ -147,7 +146,6 @@ module.exports = (db) => {
       });
   });
 
-
   // POST generate password
   router.post("/accounts/generate_password", (req, res) => {
     res.redirect("/api/accounts/generate_password");
@@ -170,7 +168,6 @@ module.exports = (db) => {
       });
   });
 
-
   // POST edit password
   router.post("/accounts/:accountId", (req, res) => {
     console.log("HERE", req.body);
@@ -185,7 +182,7 @@ module.exports = (db) => {
       [newPassword, accountId, userOrg]
     )
       .then((data) => {
-        const templateVars = {id: accountId}
+        const templateVars = { id: accountId };
         res.redirect("/api/accounts");
       })
       .catch((err) => {

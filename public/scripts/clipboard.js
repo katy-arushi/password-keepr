64e0c2a07281ac3cb0a8a0fa34efa19cac12cf43
OliviaWong-dev <olivia.wy.wong@gmail.com>
2021-10-29 00:59:13 +0000

    setup copyclipboard

---
+++ public/scripts/clipboard.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  function copyToClipboard(text) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val(text).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+
+  $("#copyButton").click(function () {
+    copyToClipboard($('input[name="manual_password"').val());
+  });
+});
